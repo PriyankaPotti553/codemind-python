d2a0b52d79acb1458cc44410ac795b24cf6c4e01
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2023-05-21 09:58:56 +0530

    n=int(input()) if n<3:     print("The pattern is not possible") else:     for i in range(1,n+1):         for j in range(1,i+1):             print('*',end="")         print()     for i in range(n-1,0,-1):         for j in range(i,0,-1):             print('*',end="")         print("")

--- Half_diamond_star_pattern.py
+++ Half_diamond_star_pattern.py
@@ -5,9 +5,8 @@ else:
     for i in range(1,n+1):
         for j in range(1,i+1):
             print('*',end="")
-        print("")
+        print()
     for i in range(n-1,0,-1):
         for j in range(i,0,-1):
             print('*',end="")
         print("")
-      
